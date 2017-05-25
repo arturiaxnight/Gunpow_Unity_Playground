09b5cc48773a212e153acc6bf6e0a2934026c7ec
Gunpow_TW <arturiaxnight@gmail.com>
2017-05-25 11:15:04 +0800

    版面布置
    製作完成版面
    開場擺放珠子

---
+++ Gunpow_ball/Assets/Scripts/BallControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+    public Sprite[] balls;
+	// Use this for initialization
+	void Start () {
+        SpawnRandomBall();
+	}
+	void SpawnRandomBall()
+    {
+        foreach (SpriteRenderer child in GetComponentsInChildren<SpriteRenderer>())
+        {
+            if (child.transform == transform)
+                continue;
+            child.sprite = balls[Random.Range(0, 5)];
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+            Application.LoadLevel(Application.loadedLevel);
+	}
+}
